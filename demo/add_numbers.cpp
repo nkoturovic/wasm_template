7a4e47a55e7c12096a095d2cf8361a9670bc7120
Nebojša Koturović <necer95@gmail.com>
2021-12-09 16:23:05 +0100

    Update functions.cpp

--- demo/add_numbers.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "functions.hpp"
-
-int main() {
-	std::cout << add(1,2) << '\n';
-	return 0;
-}
--- src/functions.cpp
+++ src/functions.cpp
@@ -1,4 +1,9 @@
-#include "functions.hpp"
+#include <emscripten/val.h>
+#include <inttypes.h>
 
+thread_local const val Uint8Array = val::global("Uint8Array");
 
-int add(int a, int b) { return a + b; }
+val processImage(std::string image_in) {
+  uint8_t *image_buffer = static_cast<uint8_t>(image_in.c_str());
+  return int8Array.new_(typed_memory_view(image_buffer, 256));
+}
