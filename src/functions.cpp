277f3ac940e0266f15b8b2e09d4538d231e07aeb
Nebojsa Koturovic <contact@kotur.me>
2021-12-09 18:29:37 +0100

    Add conditional compilation

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,9 +1,30 @@
+#include <string>
+#include <iostream>
+
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
 #include <emscripten/val.h>
 #include <inttypes.h>
+#include <string>
+#include <iostream>
+
+using namespace emscripten;
 
 thread_local const val Uint8Array = val::global("Uint8Array");
 
-val processImage(std::string image_in) {
-  uint8_t *image_buffer = static_cast<uint8_t>(image_in.c_str());
-  return int8Array.new_(typed_memory_view(image_buffer, 256));
+EMSCRIPTEN_KEEPALIVE
+extern "C" int processImage(std::string image_in) {
+  uint8_t * image_buffer = (uint8_t*)(image_in.c_str());
+//   std::cout << image_buffer << '\n';
+  return 3;
 }
+
+#else 
+int processImage(std::string image_in) {
+	return 0;
+}
+
+int main() {
+	return 0;
+}
+#endif
