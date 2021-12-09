15e1d83b5a37f48bb1d38d4b5e9d1697b7f6d464
Nebojsa Koturovic <contact@kotur.me>
2021-12-09 20:15:17 +0100

    Prepare for mozjpeg inclusion

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,6 +1,36 @@
 #include <string>
 #include <iostream>
 
+/* Mozjpeg stuff */
+#include <jpeglib.h>
+
+
+#include <turbojpeg.h>
+
+void compress() {
+	const int JPEG_QUALITY = 75;
+	const int COLOR_COMPONENTS = 3;
+	int _width = 1920;
+	int _height = 1080;
+	long unsigned int _jpegSize = 0;
+	unsigned char* _compressedImage = NULL; //!< Memory is allocated by tjCompress2 if _jpegSize == 0
+	unsigned char buffer[_width*_height*COLOR_COMPONENTS]; //!< Contains the uncompressed image
+
+	tjhandle _jpegCompressor = tjInitCompress();
+
+	tjCompress2(_jpegCompressor, buffer, _width, 0, _height, TJPF_RGB,
+		  &_compressedImage, &_jpegSize, TJSAMP_444, JPEG_QUALITY,
+		  TJFLAG_FASTDCT);
+
+	tjDestroy(_jpegCompressor);
+
+	//to free the memory allocated by TurboJPEG (either by tjAlloc(),
+	//or by the Compress/Decompress) after you are done working on it:
+	//tjFree(&_compressedImage);
+	tjFree(_compressedImage);
+}
+
+
 #ifdef EMSCRIPTEN
 #include <emscripten.h>
 #include <emscripten/val.h>
@@ -16,6 +46,9 @@ EMSCRIPTEN_KEEPALIVE
 extern "C" int processImage(std::string image_in) {
   uint8_t * image_buffer = (uint8_t*)(image_in.c_str());
 //   std::cout << image_buffer << '\n';
+//
+	
+  compress();
   return 3;
 }
 
